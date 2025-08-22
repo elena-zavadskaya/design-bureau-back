5e5a3b66faad17c3aab8921477d65feed5512e46
elena-zavadskaya <ele.zavadskaya.na@gmail.com>
2025-08-23 02:57:25 +0300

    Fix2

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -142,6 +142,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Настройки WhiteNoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 WHITENOISE_USE_FINDERS = True
