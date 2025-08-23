83f63a2d0a87203b96049587844a40b6163df0e2
elena-zavadskaya <ele.zavadskaya.na@gmail.com>
2025-08-23 05:18:25 +0300

    Fix12

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=DEBUG
+        ssl_require=True
     )
 }
 
